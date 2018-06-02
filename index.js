0733c2cca1b21fbc73c28850820e096fbf088a1e
PratyushSingh06051998 <pratyushsingh.6may@gmail.com>
2018-06-02 13:53:01 +0530

    D

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function InsertFinalValue(req,res,id){
   var REGISTRATION_YEAR =  req.body.registeryear;
   var EXPERIENCE =  parseInt(req.body.experience);
 
-  var sql = 'INSERT INTO doctor_master (dm_doctor_id, dm_doctor_name, dm_gender, dm_doctor_contact_mobile, dm_doctor_speciality_id, dm_doctor_email, dm_medical_registration_number, dm_registration_council, dm_registration_year, dm_doctor_experience) VALUES ((?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?))';
+  var sql = 'INSERT INTO doctor_master (dm_doctor_id, dm_doctor_name, dm_gender, dm_doctor_contact_mobile, dm_doctor_speciality_id, dm_doctor_email, dm_medical_registration_number, dm_registration_council, dm_registration_year, dm_doctor_experience) VALUES ((?),(?),(?),(?),(?),(?),(?),(?),(?),(?))';
   var sql1 = 'INSERT INTO partner_login_details_master (pld_role, pld_username, pld_password, pld_partner_id, pld_mobile) VALUES ((?),(?),(?),(?),(?))';
 
 
